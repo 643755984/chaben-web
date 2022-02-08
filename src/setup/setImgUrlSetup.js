88adfcb4dbf674d362166788753eb721f9241768
643755984 <643755984@qq.com>
2022-02-08 21:28:12 +0800

    设置默认图片

--- src/setup/setImgUrlSetup.js
+++ src/setup/setImgUrlSetup.js
@@ -1,8 +1,10 @@
+import noImg from '@/assets/img/noImg.jpg'
+
 export default function setImgUrl() {
     const url = import.meta.env.VITE_BASE_API
 
     const setImgUrl = (str) => {
-        return url + str
+        return str ? url + str : noImg
     }
 
     return {
